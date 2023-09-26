b1b4798c410275f181d2fbe8bcf925d6202c2c35
CaroGonza <natalia.carolina.gonzalez@gmail.com>
2023-09-26 00:42:36 -0300

    cards

--- js/inicio.js
+++ js/inicio.js
@@ -1,54 +1,66 @@
 let productos = [
-    { nombre: 'TV', precio: 145154, stock: 44, marca: 'Samsung', categoria: 'Computación', detalles: 'detalles del TV...', envio: false, foto: 'https://samsungar.vtexassets.com/arquivos/ids/176324-800-auto?width=800&height=auto&aspect=true' },
-    { nombre: 'Mouse', precio: 4567, stock: 65, marca: 'Genius', categoria: 'Computación', detalles: 'detalles del mouse...', envio: true, foto: 'https://tienda.anywayinsumos.com.ar/15425-thickbox_default/mouse-genius-usb-dx-110-120-rojo.jpg' },
-    { nombre: 'Mesa', precio: 8765, stock: 100, marca: 'Pinolandia', categoria: 'Hogar', detalles: 'Mesa de pino', envio: true, foto: 'https://http2.mlstatic.com/D_NQ_NP_790402-MLA70515144291_072023-O.webp' },
-    { nombre: 'Heladera', precio: 34567, stock: 23, marca: 'Philips', categoria: 'Electrodoméstico', detalles: 'Color blanca', envio: false, foto: 'https://mancihogar.com.ar/wp-content/uploads/2022/10/370.jpg' },
-    { nombre: 'Memoria DDR', precio: 65432, stock: 78, marca: 'Kingston', categoria: 'Computación', detalles: '16GB', envio: true, foto: 'https://www.totalcompu.com.ar/image/thumbnails/18/8f/memoria_ddr_1gb_kingston_con_gtia_zona_sur_oferta_1247_MLA4742383680_072013_F_1__jpg-100599-600x600.jpg' },  
-    { nombre: 'TV', precio: 1, stock: 44, marca: 'Samsung', categoria: 'Computación', detalles: 'detalles del TV...', envio: false, foto: 'https://samsungar.vtexassets.com/arquivos/ids/176324-800-auto?width=800&height=auto&aspect=true' },
-    { nombre: 'Mouse', precio: 4567, stock: 65, marca: 'Genius', categoria: 'Computación', detalles: 'detalles del mouse...', envio: true, foto: 'https://tienda.anywayinsumos.com.ar/15425-thickbox_default/mouse-genius-usb-dx-110-120-rojo.jpg' },
-    { nombre: 'Mesa', precio: 8765, stock: 100, marca: 'Pinolandia', categoria: 'Hogar', detalles: 'Mesa de pino', envio: true, foto: 'https://http2.mlstatic.com/D_NQ_NP_790402-MLA70515144291_072023-O.webp' },
-    { nombre: 'Heladera', precio: 34567, stock: 23, marca: 'Philips', categoria: 'Electrodoméstico', detalles: 'Color blanca', envio: false, foto: 'https://mancihogar.com.ar/wp-content/uploads/2022/10/370.jpg' },
-    { nombre: 'Memoria DDR', precio: 65432, stock: 78, marca: 'Kingston', categoria: 'Computación', detalles: '16GB', envio: true, foto: 'https://www.totalcompu.com.ar/image/thumbnails/18/8f/memoria_ddr_1gb_kingston_con_gtia_zona_sur_oferta_1247_MLA4742383680_072013_F_1__jpg-100599-600x600.jpg' },
-    { nombre: 'TV', precio: 1, stock: 44, marca: 'Samsung', categoria: 'Computación', detalles: 'detalles del TV...', envio: false, foto: 'https://samsungar.vtexassets.com/arquivos/ids/176324-800-auto?width=800&height=auto&aspect=true' },
-    { nombre: 'Mouse', precio: 4567, stock: 65, marca: 'Genius', categoria: 'Computación', detalles: 'detalles del mouse...', envio: true, foto: 'https://tienda.anywayinsumos.com.ar/15425-thickbox_default/mouse-genius-usb-dx-110-120-rojo.jpg' },
-    { nombre: 'Mesa', precio: 8765, stock: 100, marca: 'Pinolandia', categoria: 'Hogar', detalles: 'Mesa de pino', envio: true, foto: 'https://http2.mlstatic.com/D_NQ_NP_790402-MLA70515144291_072023-O.webp' },
-    { nombre: 'Heladera', precio: 34567, stock: 23, marca: 'Philips', categoria: 'Electrodoméstico', detalles: 'Color blanca', envio: false, foto: 'https://mancihogar.com.ar/wp-content/uploads/2022/10/370.jpg' },
-    { nombre: 'Memoria DDR', precio: 65432, stock: 78, marca: 'Kingston', categoria: 'Computación', detalles: '16GB', envio: true, foto: 'https://www.totalcompu.com.ar/image/thumbnails/18/8f/memoria_ddr_1gb_kingston_con_gtia_zona_sur_oferta_1247_MLA4742383680_072013_F_1__jpg-100599-600x600.jpg' },  
-    { nombre: 'TV', precio: 1, stock: 44, marca: 'Samsung', categoria: 'Computación', detalles: 'detalles del TV...', envio: false, foto: 'https://samsungar.vtexassets.com/arquivos/ids/176324-800-auto?width=800&height=auto&aspect=true' },
-    { nombre: 'Mouse', precio: 4567, stock: 65, marca: 'Genius', categoria: 'Computación', detalles: 'detalles del mouse...', envio: true, foto: 'https://tienda.anywayinsumos.com.ar/15425-thickbox_default/mouse-genius-usb-dx-110-120-rojo.jpg' },
-    { nombre: 'Mesa', precio: 8765, stock: 100, marca: 'Pinolandia', categoria: 'Hogar', detalles: 'Mesa de pino', envio: true, foto: 'https://http2.mlstatic.com/D_NQ_NP_790402-MLA70515144291_072023-O.webp' },
-    { nombre: 'Heladera', precio: 34567, stock: 23, marca: 'Philips', categoria: 'Electrodoméstico', detalles: 'Color blanca', envio: false, foto: 'https://mancihogar.com.ar/wp-content/uploads/2022/10/370.jpg' },
-    { nombre: 'Memoria DDR', precio: 65432, stock: 78, marca: 'Kingston', categoria: 'Computación', detalles: '16GB', envio: true, foto: 'https://www.totalcompu.com.ar/image/thumbnails/18/8f/memoria_ddr_1gb_kingston_con_gtia_zona_sur_oferta_1247_MLA4742383680_072013_F_1__jpg-100599-600x600.jpg' }, 
-    { nombre: 'Mouse', precio: 4567, stock: 65, marca: 'Genius', categoria: 'Computación', detalles: 'detalles del mouse...', envio: true, foto: 'https://tienda.anywayinsumos.com.ar/15425-thickbox_default/mouse-genius-usb-dx-110-120-rojo.jpg' },
-    { nombre: 'Mesa', precio: 8765, stock: 100, marca: 'Pinolandia', categoria: 'Hogar', detalles: 'Mesa de pino', envio: true, foto: 'https://http2.mlstatic.com/D_NQ_NP_790402-MLA70515144291_072023-O.webp' },
-    { nombre: 'Heladera', precio: 34567, stock: 23, marca: 'Philips', categoria: 'Electrodoméstico', detalles: 'Color blanca', envio: false, foto: 'https://mancihogar.com.ar/wp-content/uploads/2022/10/370.jpg' },
-    { nombre: 'Memoria DDR', precio: 65432, stock: 78, marca: 'Kingston', categoria: 'Computación', detalles: '16GB', envio: true, foto: 'https://www.totalcompu.com.ar/image/thumbnails/18/8f/memoria_ddr_1gb_kingston_con_gtia_zona_sur_oferta_1247_MLA4742383680_072013_F_1__jpg-100599-600x600.jpg' }, 
+    { nombre: 'Cuaderno Escolar', precio: 4900, categoria: 'Cuadernos', detalles: 'Cuadriculado 21x27cm. Caritas.', foto: './IMG/Fotos ecommerce/1-cuaderno-caritas.png' },
+    { nombre: 'Cuaderno Escolar', precio: 4900, categoria: 'Cuadernos', detalles: 'Liso 21x27cm. Celeste.', foto: './IMG/Fotos ecommerce/2-cuaderno-liso-celeste.png' },
+    { nombre: 'Cuaderno Escolar', precio: 2618, categoria: 'Cuadernos', detalles: 'Cuaderno Rayado A4. Unicorn ', foto: './IMG/Fotos ecommerce/cuaderno-unicornio.png' },
+    { nombre: 'Cuaderno Universitario', precio: 5060, categoria: 'Cuadernos', detalles: 'Cuaderno Rayado A4. Trust. ', foto: './IMG/Fotos ecommerce/cuadernotrust-rayado.png' },
+    { nombre: 'Cuaderno Universitario', precio: 5060, categoria: 'Cuadernos', detalles: 'Cuaderno Rayado A4. Choose. ', foto: './IMG/Fotos ecommerce/cuaderno-A4-Choose.png' },
+    { nombre: 'Cuaderno Harry Potter', precio: 3600, categoria: 'Cuadernos', detalles: 'Cuaderno Rayado con espiral A4. ', foto: './IMG/Fotos ecommerce/cuaderno-harry-potter-2.png'},
+    { nombre: 'Lápices Acuarelados', precio: 3170, categoria: 'Lápices', detalles: 'Lápices de Colores Acuarelados x 12. ', foto: './IMG/Fotos ecommerce/8-lapices-acuarelables.png'},
+    { nombre: 'Lápices Flúo', precio: 1975, categoria: 'Lápices', detalles: 'Lápices de Colores Pinto Fluo x 8', foto: './IMG/Fotos ecommerce/pinto-fluo.png'}, 
+    { nombre: 'Lápices con goma', precio: 579, categoria: 'Lápices', detalles: 'Stay Wild. Animales. Color verde x 1', foto: './IMG/Fotos ecommerce/10-lapiz-con-goma.png'}, 
+    { nombre: 'Lápices de colores', precio: 3969, categoria: 'Lápices', detalles: 'Lápices Pasteles Pastel Tentación x 20 ', foto: './IMG/Fotos ecommerce/lapiz-pastel.png'}, 
+    { nombre: 'Lápices de colores', precio: 17356, categoria: 'Lápices', detalles: 'Mooving Lata Todos tus Colores x 48', foto: './IMG/Fotos ecommerce/lata-por-48.png'}, 
+    { nombre: 'Lápices de colores', precio: 3789, categoria: 'Lápices', detalles: 'Lápices de colores Colors 3.0 x 24.', foto: './IMG/Fotos ecommerce/giotto-3.0-por-24.png'},
+    { nombre: 'Marcadores Sharpie', precio: 4114, categoria: 'Marcadores', detalles: 'Vintage Travel x 8 ', foto: './IMG/Fotos ecommerce/12-blister-por-8.png'},
+    { nombre: 'Marcadores Glitter', precio: 3472, categoria: 'Marcadores', detalles: 'Colores pastel x 6 ', foto: './IMG/Fotos ecommerce/mooving.png'},
+    { nombre: 'Marcadores Sharpie', precio: 5989, categoria: 'Marcadores', detalles: 'Punta Fina. Tropical x 12. ', foto: './IMG/Fotos ecommerce/sharpie-tropical.png'},
+    { nombre: 'Marcadores Filgo', precio: 6498, categoria: 'Marcadores', detalles: 'Marker 036 x 20. ', foto: './IMG/Fotos ecommerce/marker-036.png'},
+    { nombre: 'Marcadores Glitter', precio: 4774, categoria: 'Marcadores', detalles: 'Marcador Turbo Glitter x 8. ', foto: './IMG/Fotos ecommerce/TURBO-GLITTER.png'},
+    { nombre: 'Marcadores Jumbo', precio: 2939, categoria: 'Marcadores', detalles: 'Marcador Pinto Jumbo x 10. ', foto: './IMG/Fotos ecommerce/jumbo.png'},
+    { nombre: 'Perforadora', precio: 2924, categoria: 'Crafting', detalles: 'Artistica 3/4". Huella', foto: './IMG/Fotos ecommerce/PERFORADORA-PISADA.png'},
+    { nombre: 'Perforadora', precio: 4463, categoria: 'Crafting', detalles: 'Artistica 1". Mariposa', foto: './IMG/Fotos ecommerce/perforadora-relieve-mariposa.png'},
+    { nombre: 'Perforadora', precio: 4463, categoria: 'Crafting', detalles: 'Artistica 2". Estampilla', foto: './IMG/Fotos ecommerce/PERFORADORA-ESTAMPILLA.png'},
+    { nombre: 'Perforadora', precio: 12496, categoria: 'Crafting', detalles: 'Artistica 2". Estrella.', foto: './IMG/Fotos ecommerce/perforadora-estrela-5-cm.png'},
+    { nombre: 'Perforadora', precio: 4304, categoria: 'Crafting', detalles: 'Pinza Estrella 1/4". Estrella', foto: './IMG/Fotos ecommerce/PINZA-ESTRELLA.png'},
+    { nombre: 'Cutter', precio: 3020, categoria: 'Crafting', detalles: 'Mango ergonómico. Blanco', foto: './IMG/Fotos ecommerce/11-BISTURI-BLANCO.png'},
+    { nombre: 'Tijera', precio: 839, categoria: 'Crafting', detalles: 'Artística. Corte con forma. 6-1/2', foto: './IMG/Fotos ecommerce/tijera.png'},
+    { nombre: 'Cartulina', precio: 2159, categoria: 'Papeleria', detalles: 'Flúor. A4. Colores surtidos x 16', foto: './IMG/Fotos ecommerce/CARTULINA-FLUO.png'},
+    { nombre: 'Papel Misionero', precio: 3470, categoria: 'Papeleria', detalles: 'A4. 125gr. x 100 hojas', foto: './IMG/Fotos ecommerce/CRAFT-125-GRS.png'},
+    { nombre: 'Papel Ilustrado', precio: 9951, categoria: 'Papeleria', detalles: '30x30cm 180grs x 24', foto: './IMG/Fotos ecommerce/papeles-ilustrados.png'},
+    { nombre: 'Papel Ilustrado', precio: 4135, categoria: 'Papeleria', detalles: '15x20cm 180grs x 24', foto: './IMG/Fotos ecommerce/papel-ilustrado-correo-bonito.png'},
+    { nombre: 'Papeles Pastel', precio: 9134, categoria: 'Papeleria', detalles: '30x30cm 160grs x 35', foto: './IMG/Fotos ecommerce/PAPELES-PASTELES.png'},
+    { nombre: 'Resaltador Sharpie', precio: 5694, categoria: 'Resaltadores', detalles: 'Snote x 12 unidades.', foto: './IMG/Fotos ecommerce/s-note-12.png'},
+    { nombre: 'Resaltador Sharpie', precio: 7864, categoria: 'Resaltadores', detalles: 'Sharpie Tank + Pocket x16', foto: './IMG/Fotos ecommerce/sharpie-tank.png'},
+    { nombre: 'Resaltador Filgo', precio: 4170, categoria: 'Resaltadores', detalles: 'Creativo Lighter Fun x 12', foto: './IMG/Fotos ecommerce/ligther-fun.png'},
+    { nombre: 'Acuarelas', precio: 6642, categoria: 'Escolar', detalles: '36 Colores Bandeja    + Pincel', foto: './IMG/Fotos ecommerce/acuarelas.png'},
+    { nombre: 'Notas adhesivas', precio: 1042, categoria: 'Escolar', detalles: 'Maw Mania Neon 51x51', foto: './IMG/Fotos ecommerce/cubo-neon.png'},
+    { nombre: 'Binder Clips', precio: 1276, categoria: 'Escolar', detalles: 'Pastel. 25mm x 6 unidades', foto: './IMG/Fotos ecommerce/clips-25mm.png'},
+    { nombre: 'Binder Clips', precio: 1387, categoria: 'Escolar', detalles: 'Pastel 19mm x 12 unidades', foto: './IMG/Fotos ecommerce/clips-19mm.png'},
+    { nombre: 'Señaladores', precio: 809, categoria: 'Escolar', detalles: 'Banderitas Pastel x 8 unidades.', foto: './IMG/Fotos ecommerce/banderitas.png'},
+    { nombre: 'Broches', precio: 1496, categoria: 'Escolar', detalles: 'Madera. 25mm x 50. Colores surtidos.', foto: './IMG/Fotos ecommerce/13-broche-color.png'},
 ]
 
 
 let productosDestacados = [
-    { nombre: 'TV', precio: 1, stock: 44, marca: 'Samsung', categoria: 'Computación', detalles: 'detalles del TV...', envio: false, foto: 'https://samsungar.vtexassets.com/arquivos/ids/176324-800-auto?width=800&height=auto&aspect=true' },
-    { nombre: 'Mouse', precio: 4567, stock: 65, marca: 'Genius', categoria: 'Computación', detalles: 'detalles del mouse...', envio: true, foto: 'https://tienda.anywayinsumos.com.ar/15425-thickbox_default/mouse-genius-usb-dx-110-120-rojo.jpg' },
-    { nombre: 'Mesa', precio: 8765, stock: 100, marca: 'Pinolandia', categoria: 'Hogar', detalles: 'Mesa de pino', envio: true, foto: 'https://http2.mlstatic.com/D_NQ_NP_790402-MLA70515144291_072023-O.webp' },
-    { nombre: 'Heladera', precio: 34567, stock: 23, marca: 'Philips', categoria: 'Electrodoméstico', detalles: 'Color blanca', envio: false, foto: 'https://mancihogar.com.ar/wp-content/uploads/2022/10/370.jpg' },
-    { nombre: 'Memoria DDR', precio: 65432, stock: 78, marca: 'Kingston', categoria: 'Computación', detalles: '16GB', envio: true, foto: 'https://www.totalcompu.com.ar/image/thumbnails/18/8f/memoria_ddr_1gb_kingston_con_gtia_zona_sur_oferta_1247_MLA4742383680_072013_F_1__jpg-100599-600x600.jpg' },  
+    { nombre: 'Cuaderno Escolar', precio: 4900, categoria: 'Cuadernos', detalles: 'Cuadriculado 21x27cm. Caritas.', foto: './IMG/Fotos ecommerce/1-cuaderno-caritas.png' },
+    { nombre: 'Cuaderno Escolar', precio: 4900, categoria: 'Cuadernos', detalles: 'Liso 21x27cm. Celeste.', foto: './IMG/Fotos ecommerce/2-cuaderno-liso-celeste.png' },
+    { nombre: 'Cuaderno Escolar', precio: 2618, categoria: 'Cuadernos', detalles: 'Cuaderno Rayado A4. Unicorn ', foto: './IMG/Fotos ecommerce/cuaderno-unicornio.png' },
 ]
+
+representarCards()
+representarCardsDestacadas()
+
 
 function representarCards() {
     for(let i=0; i<productos.length; i++) {
-        document.querySelector('.cards-container').innerHTML += 
+        document.querySelector('.cards-container').insertAdjacentHTML ("afterbegin", 
             '<section id="cards">' +
                 '<img src="' + productos[i].foto + '" alt="">' +
                 '<p style="font-variant:small-caps; font-size:0.8em; color:#ADADAD;"> '+ productos[i].categoria +'</p>' +
-                '<p><span style="font-variant:small-caps; font-size:1.1em; font-weight:bold;letter-spacing: 0.1em;">'+ productos[i].nombre + '</span>: '+  productos[i].detalles  +'</p>' +
-                '<p style="font-size:1.3em; color:#ec5151; margin-top:0.3em"><b>$</b>'+ productos[i].precio +'</p>' +
-                '<br>' +
-                /* "<div><button><b>Agregar al Carrito</b>" + "</button>" +
-                "<a href='./favoritos.html'><img id='fav' src='./IMG/favoritos.png'alt='Favoritos'></a>" + "</div>" */
-                "<button><b id='btn-carrito-cards'>Agregar al carrito</b>             <span class='icon-heart' style='font-size: 1.4em;'></span></button>" +
-                 
-                
-            '</section>'
+                '<p style="margin-left:0.5em; margin-right:0.5em; "><span style="font-variant:small-caps; font-size:1em; font-weight:bold;">'+ productos[i].nombre + '</span>: '+  productos[i].detalles  +'</p>' +
+                '<p style="font-size:1.3em; font-weight:bold; color:#ec5151; margin-top:0.2em">$'+ productos[i].precio +'</p>' +
+                "<div id='botones'><button id='cart'><b>Agregar al Carrito</b></button><button id='fav'><i class='fa-regular fa-heart'></i></button></div>" +  
+            '</section>')
     }
 }
 
@@ -57,18 +69,26 @@ function representarCardsDestacadas() {
     for(let i=0; i<productosDestacados.length; i++) {
         document.querySelector('.cards-container-destacadas').innerHTML += 
             '<section id="cards-destacadas">' +
-                '<img src="' + productosDestacados[i].foto + '" alt="">' +
-                '<p style="font-variant:small-caps;"> '+ productosDestacados[i].categoria +'</p>' +
-                '<h3>'+ productosDestacados[i].nombre  +'</h3>' +
-                '<p> '+ productosDestacados[i].detalles +'</p>' +
-                '<p> $'+ productosDestacados[i].precio +'</p>' +
-                '<br>' +
-                '<button><b>Agregar al Carrito</b> '+'</button>' +
-                "<a href='./favoritos.html'><img src='./IMG/favoritos.png' alt='Favoritos'></a>"
+            '<img src="' + productosDestacados[i].foto + '" alt="">' +
+            '<p><span style="font-variant:small-caps; font-size:0.7em; font-weight:bold;">'+ productosDestacados[i].nombre +'</p>' +
+            '<p style="font-size:0.8em; font-weight:bold; color:#ec5151; margin-top:0.3em">$'+ productosDestacados[i].precio +'</p>' +
+            '<br>' +
+            "<div id='botones-d'><a href='http://www.google.com' <button id='cart-d'><i class='fa-solid fa-cart-shopping'></i></button></a><button id='fav-d'><i class='fa-regular fa-heart'></i></button></div>" + 
             '</section>'
     }
 }
-representarCards()
-representarCardsDestacadas()
+
+//no funca
+let btnFav = document.querySelectorAll("#fav")
+let btnCart = document.querySelectorAll("#cart")
+
+btnFav.addEventListener("click", function(){
+    console.log("apretee")
+    btnFav.innerHTML = "<i class='fa-solid fa-heart'></i>"
+    let p = document.createElement("p")
+    p.textContent =+ "meti un favorito"
+    document.body.appendChild(p)
+     
+})
 
 
